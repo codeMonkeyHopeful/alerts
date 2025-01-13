400f2d36ce77df6fd62c2c026ef90d7710aaefc1
Ryan Jasinski <jasinski.17@gmail.com>
2025-01-13 00:43:10 -0800

    working need to add documentation

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-port = 587  # For starttls
-smtp_server = "smtp.gmail.com"
 sender_email = os.getenv("SEND_EMAIL")
-sender_pass = os.getenv("SENDER_EMAIL_PASS")
+sender_pass = os.getenv("APP_PW")
 receiver_email = os.getenv("RECEIVER_EMAIL")
 
 message = """
-Sending test email from script
+Put your test message here.
 """
 
-context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)
-    server.ehlo()  # Can be omitted
-    server.login(sender_email, sender_pass)
-    server.sendmail(sender_email, receiver_email, message)
+try:
+    server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+
+    server_ssl.login(sender_email, sender_pass)  
+
+    server_ssl.sendmail(sender_email, receiver_email, message)
+
+    server_ssl.close()
+except Exception as e:
+    print(e)
