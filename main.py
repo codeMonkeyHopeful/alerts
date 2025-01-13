857466217722a22d0a761555e52bd33b57f423b0
Ryan Jasinski <jasinski.17@gmail.com>
2025-01-12 23:17:15 -0800

    pulling in env variales to use

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
+import os, smtplib, ssl
 from dotenv import load_dotenv
-import smtplib, ssl
 
 load_dotenv()
 
 
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
-sender_email = "my@gmail.com"
-receiver_email = "your@gmail.com"
-password = input("Type your password and press enter:")
-message = """\
-Subject: Hi there
+sender_email = os.getenv("SEND_EMAIL")
+sender_pass = os.getenv("SENDER_EMAIL_PASS")
+receiver_email = os.getenv("RECEIVER_EMAIL")
 
-This message is sent from Python."""
+message = """
+Sending test email from script
+"""
 
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
     server.ehlo()  # Can be omitted
     server.starttls(context=context)
     server.ehlo()  # Can be omitted
-    server.login(sender_email, password)
+    server.login(sender_email, sender_pass)
     server.sendmail(sender_email, receiver_email, message)
